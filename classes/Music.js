4a2b020ab4a5bb148917e99119d1396c72acb427
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-10 11:25:38 -0400

    Update: Music class complete w/ passing tests

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,22 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Music class:
+class Music extends Media {
+  constructor(title, year, genre, artist, length) {
+    super(title, year, genre);
+    this.artist = artist;
+    this.length = length;
+  }
 
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+  }
+
+  static shortestAlbum(songs) {
+    return songs.reduce((shortestSong, currSong) =>
+      currSong.length < shortestSong.length ? currSong : shortestSong
+    );
+  }
+}
 // don't change below
 module.exports = Music;
