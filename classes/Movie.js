d00e205c215e4babfa2b6ab5f314e05a47b1139f
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-10 11:20:20 -0400

    Update: Movies class complete w/ passing tests

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,25 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Movie class:
+class Movie extends Media {
+  constructor(title, year, genre, director, duration, rating) {
+    super(title, year, genre);
+    this.director = director;
+    this.duration = duration;
+    this.rating = rating;
+  }
 
+  summary() {
+    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static longestMovie(movies) {
+    return movies.reduce((longestMovieDuration, currMovie) =>
+      currMovie.duration > longestMovieDuration.duration
+        ? currMovie
+        : longestMovieDuration
+    );
+  }
+}
 // don't change below
 module.exports = Movie;
