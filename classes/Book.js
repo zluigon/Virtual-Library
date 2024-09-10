3a8d8fc57cdd5dc0f6d2381548dc7e9cd3068076
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-10 11:15:00 -0400

    Update: Media and Book clases complete w/ passing tests

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,26 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Book class:
+class Book extends Media {
+  constructor(title, year, genre, author, numPages, rating) {
+    super(title, year, genre);
 
+    this.author = author;
+    this.numPages = numPages;
+    this.rating = rating;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static highestRating(books) {
+    return books.reduce((highestRatedBook, currentBook) =>
+      currentBook.rating > highestRatedBook.rating
+        ? currentBook
+        : highestRatedBook
+    );
+  }
+}
 // don't change below
 module.exports = Book;
--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,19 @@
 // create your Media class:
+class Media {
+  constructor(title, year, genre) {
+    this.title = title;
+    this.year = year;
+    this.genre = genre;
+
+    Media.totalMediaCount += 1;
+  }
+
+  static totalMediaCount = 0;
+
+  summary() {
+    return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`;
+  }
+}
 
 // uncomment below to export it:
 module.exports = Media;
